1ce59f4e89f6d9613f6c3b5ebbf4a3ed1412c9df
mirahadsumon994-gif <mirahadsumon994@gmail.com>
2026-09-08 07:45:16 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.request import HTTPXRequest
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'হ্যালো {update.effective_user.first_name}! আমি tulip_padc_bot।')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'হ্যালো {update.effective_user.first_name}! আমি সচল আছি।')
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+    request = HTTPXRequest(connect_timeout=30.0, read_timeout=30.0)
+    
+    app = ApplicationBuilder().token(TOKEN).request(request).build()
     app.add_handler(CommandHandler("start", start))
+    
     print("বট সফলভাবে চালু হয়েছে...")
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
+
